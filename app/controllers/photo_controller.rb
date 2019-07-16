f8f926b9e267c975a1dc672fcdbd7ba110f60702
Himynameislou <louie1598@gmail.com>
2019-07-16 04:38:54 +0000

    L 39 struggles

--- app/controllers/photo_controller.rb
+++ app/controllers/photo_controller.rb
@@ -3,13 +3,13 @@ before_action :authenticate_user!
 
   def create
     @place = Place.find(params[ :place_id])
-    @place.caption.create(caption_params.merge(user: current_user))
+    @place.photo.create(photo_params.merge(user: current_user))
     redirect_to place_path(@place)
   end
 
   private
 
-  def caption_params
-    params.require(:caption).permit(:message)
+  def photo_params
+    params.require(:photo).permit(:message)
   end
 end
