e445dcaaa2af83ecad5c5fc7058bb854f7f220e0
Himynameislou <louie1598@gmail.com>
2019-07-17 22:46:24 +0000

    trying to fix picture not uploading

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,7 @@
 class Photo < ApplicationRecord
     belongs_to :user
     belongs_to :place
-    has_many :photo
+    #has_many :photo
     mount_uploader :picture, PictureUploader
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,12 @@ Rails.application.routes.draw do
   
   resources :places do
     resources :comments, only: :create
+    resources :photos, only: :create
   end
-  root 'places#photo'
-  resources :places do
-    resources :photo, only: :create
-  end
+  resources :users, only: :show
+  #root 'places#photo'
+  #resources :places do
+  #  resources :photo, only: :create
+  #end
 
 end
