425eaab8ca3d35cc181ebb27e8e41de90906125f
Himynameislou <louie1598@gmail.com>
2019-07-18 00:23:30 +0000

    TA help part 2

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,14 +4,14 @@ before_action :authenticate_user!
   def create
 #    @photo = Photo.find(params[ :place_id])
     @place = Place.find(params[ :place_id])
-    @place.photos.create(photo_params.merge(user: current_user))
+    @place.photo.create(photo_params.merge(user: current_user))
     redirect_to place_path(@place)
   end
 
   private
 
   def photo_params
-    params.require(:photos).permit(:caption, :picture)
+    params.require(:photo).permit(:caption, :picture)
   end
 end
 
