f5d0ff84bc2f60881be1e08ee88302e9aa2db7dc
Himynameislou <louie1598@gmail.com>
2019-07-23 05:58:53 +0000

    Branch, L39, got pictures to show on page with captions.  Looks sloppy so maybe need to edit that.  Now at 'Updating CarrierWave to use S3' portion of the lesson

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,6 @@
 class Photo < ApplicationRecord
-    belongs_to :user
-    belongs_to :place
+    #Commented out below bc of Step 5 of L 39
+    #belongs_to :user
+    #belongs_to :place
+  mount_uploader :picture, PictureUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_22_184446) do
+ActiveRecord::Schema.define(version: 2019_07_22_224945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 2019_07_22_184446) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "picture"
+    t.integer "user"
   end
 
   create_table "places", force: :cascade do |t|
